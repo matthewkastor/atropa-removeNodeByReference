b43805fffa2fc031a153334a55bd364df021c552
Matthew Kastor <matthewkastor@gmail.com>
2014-01-01 14:37:16 -0500

    updated docs

--- docs/vsdoc/OpenLayersAll.js
+++ docs/vsdoc/OpenLayersAll.js
@@ -18,28 +18,3 @@
 
   
 
-  
-/* vsdoc for atropa */
-
-(function (window) {
-    
-
-    window.atropa = {
-        /// <summary></summary>
-        /// <returns type="atropa"/>
-                
-        removeNodeByReference: function(elementReference) {
-            /// <summary>Removes DOM Nodes.</summary>
-            /// <param name="elementReference" type="DOM Node">A reference to the DOM Node you want
-            /// to remove.</param>
-        }
-        
-    };
-
-    var $x = window.atropa;
-    $x.__namespace = "true";
-    $x.__typeName = "atropa";
-})(this);
-
-  
-
